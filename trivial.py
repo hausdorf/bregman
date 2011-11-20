e5c207aab21c228335586be2e527e2cad4545318
John Moeller <fishcorn@gmail.com>
2011-11-20 12:49:05 -0700

    Tweaked trivial algo, added kl.py

--- trivial.py
+++ trivial.py
@@ -3,14 +3,7 @@
 import sys
 import math
 
-def KL1d(xi, yi):
-    if xi == 0.0:
-        return 0.0
-    else:
-        return xi * math.log(xi/yi)
-
-def KL(x, y):
-    return reduce(lambda s, p: s+KL1d(p[0], p[1]), zip(x,y), 0.0)
+from kl import KL
 
 def trivial(dist, it):
     curcenter = None
@@ -18,9 +11,9 @@ def trivial(dist, it):
 
     for vec in it:
         if not curcenter:
-            curcenter = vec
+            curcenter = [xi for xi in vec]
 
-        radius = max(radius, dist(vec, curcenter))
+        radius = max(radius, dist([xi for xi in vec], curcenter))
 
     return (curcenter, radius)
 
@@ -28,7 +21,7 @@ def main(argv):
     
     delim = None
 
-    (center, radius) = trivial(KL, (map(float, line.split(delim)) for line in sys.stdin))
+    (center, radius) = trivial(KL, ((float(xi) for xi in line.split(delim)) for line in sys.stdin))
 
     print center, radius
 
