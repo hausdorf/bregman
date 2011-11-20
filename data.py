b8332308750edc3929a6185302719d508d80b494
Alex Clemmer <clemmer.alexander@gmail.com>
2011-11-20 14:00:19 -0700

    Update dirichlet() to take base parameter vector

--- data.py
+++ data.py
@@ -4,16 +4,18 @@ import random
 # them generate data, some of the process data, and so on.
 
 
-# Generates n random Dirichlet samples, each with d dimensions
-def dirichlet(n, d):
+# Generates n random Dirichlet samples, with base vector parameter params
+def dirichlet(n, params):
 	for i in range(n):
+		d = len(params)
+
 		# Create d random numbers, sample from Gamma for each, then
 		# normalize to 1; this is a reasonable approximation of x ~ Dir
-		smp = [random.gammavariate(random.random(),1) for a in range(d)]
+		smp = [random.gammavariate(a,1) for a in params]
 		yield [v/sum(smp) for v in smp]
 
 
 if __name__ == '__main__':
 	# Test the Dirichlet generator
-	for d in dirichlet(3, 3):
-		print d
+	for vec in dirichlet(3, [2,3,4]):
+		print vec, sum(d)
