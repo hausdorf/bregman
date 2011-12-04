db512ddd88763216cf4b95c06ac1acabbadb158e
Alex Clemmer <clemmer.alexander@gmail.com>
2011-12-03 18:59:44 -0700

    Update ext_ball

--- ext_ball.py
+++ ext_ball.py
@@ -5,6 +5,11 @@ from cvxmod.atoms import norm1
 from cvxmod.sets import probsimp
 
 
+SET1 = 'set1.txt'
+BAD1 = 'bad.txt'
+GOOD = 'good.txt'
+
+
 def f(l, p1, p2, r):
 	return d(x(p1, p2, l), p2) - r
 
@@ -34,6 +39,7 @@ def bsearch(func, lower, upper):
 	while lower <= upper:
 		l_c = (lower + upper) / 2
 		c = func(l_c)
+		delta = upper - lower
 
 		if c == 0:
 			return l_c
@@ -44,17 +50,54 @@ def bsearch(func, lower, upper):
 
 	return None
 
+def bs(l_c, l_far, p, c):
+	res1 = d(x(p, c, l_far), x(p, c, l_c))
+	res2 = d(p, x(p, c, l_c))
+	return res1 - res2
+
+def bs2(l, p, c):
+	x_l = x(p, c, l)
+	return d(c, x_l) - d(p, x_l)
+
 # meb_kl : point c -> radius r -> point p -> point p_far
 # each point c, p, and p_far are d-dimensional
 def meb_kl(c, r, p):
 	f_of_c = f(1, p, c, r)
 	x_lamb = x(p, c, 1)
 	l_far = brentq(f, 1, 300, args=(p, c, r))
-	print d(x(p, c, l_far), c)
-	l_c = bsearch(lambda x: search_helper(l_far, x, p, c), 0, l_far)
-	if l_c:
-		print d(x(p, c, l_far), x(p, c, l_c)), d(p, x(p, c, l_c))
+	#print d(x(p, c, l_far), c)
+	l_c = brentq(bs, 0, l_far, args=(l_far, p, c))
+	#l_c = bsearch(lambda x: search_helper(l_far, x, p, c), 0, l_far)
+	#print d(x(p, c, l_far), x(p, c, l_c)), d(p, x(p, c, l_c))
+	return l_c
+
+def set1_meb_kl():
+	f = open(GOOD)
+
+	# get dimensions and num of examples
+	dim,n = map(int, f.readline().split(','))
+	S = [map(lambda x:float(x), l.split(',')) for l in f.readlines()]
+
+	c = S[0]
+	p = S[1]
+	tmp = x(p, c, brentq(bs2, 0, 1, args=(S[1], c)))
+	r = d(p, x(p, c, brentq(bs2, 0, 1, args=(S[1], c))))
+	c = tmp
+	for i in range(2, n):
+		p = S[i]
+
+		if d(p, c) < r:
+			continue
+
+		l_c = meb_kl(c, r, p)
+		c = x(p, c, l_c)
+		r = d(p, c)
+		print c, r
+
+	f.close()
+
 
 
 if __name__ == '__main__':
-	meb_kl((e,e,e,e,e), 1, (e**2, e**1.5, e**2.5, e**2, e**3))
+	#meb_kl((e,e,e,e,e), 1, (e**2, e**1.5, e**2.5, e**2, e**3))
+	set1_meb_kl()
